e8ac7b0b0816e4cf4594ccb4ec0b936c51c4c27e
Andrew <Cubified@users.noreply.github.com>
2023-02-04 19:49:27 -0800

    Fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,6 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    paths: {
-      base: dev ? '' : '/docs'
-    },
     adapter: adapter({
       pages: 'docs',
       assets: 'docs'
